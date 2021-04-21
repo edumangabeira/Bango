9047e7acc763ca1fb573e16672b69b1d10b44a50
edu <edu.mangaba@gmail.com>
2021-04-21 16:12:37 -0300

    fix: sqlalchemy plugin

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or '1senya7veryhard88'
 	TABELA_GASTOS = os.environ.get('TABELA_GASTOS')
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres", "postgresql") or \
 	'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
